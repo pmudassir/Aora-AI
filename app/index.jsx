d6a1ed131074376283882e3b715ea8977096471a
pmudassir <pmuddasir@gmail.com>
2024-04-23 22:49:43 +0530

    working on auth

--- app/index.jsx
+++ app/index.jsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full items-center justify-center min-h-[84vh] px-4">
+        <View className="w-full items-center justify-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
