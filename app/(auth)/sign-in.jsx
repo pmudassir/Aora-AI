b29acfd227c7a7c9ecdf9c1fcf096d35b4a360db
pmudassir <pmuddasir@gmail.com>
2024-04-27 20:15:33 +0530

    working on state management

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import FormField from '../../components/FormField'
 import { images } from '../../constants'
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import { signIn } from '../../lib/appwrite'
 const SignIn = () => {
   const [form, setForm] = useState({
     email: '',
@@ -12,7 +13,22 @@ const SignIn = () => {
   })
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = () => { }
+  const submit = async () => {
+    if (form.email === "" || form.password === "") {
+      Alert.alert('Error', 'Please fill in all fields')
+    }
+
+    setIsSubmitting(true)
+
+    try {
+      const result = await signIn(form.email, form.password)
+      router.replace("/home")
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
 
   return (
     <SafeAreaView className="bg-primary h-full">
---
+++ context/GlobalProvider.js
@@ -0,0 +1,4 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const GlobalContext = createContext();
+export const useGlobalContext = () => useContext(GlobalContext);
